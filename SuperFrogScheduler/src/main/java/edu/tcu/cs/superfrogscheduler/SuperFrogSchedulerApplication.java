db548bdbe11784d548dff2d6b34361c8085ef196
John Nguyen <j4nguyen134@gmail.com>
2023-04-08 16:00:35 -0500

    initial backend commit

---
+++ SuperFrogScheduler/src/main/java/edu/tcu/cs/superfrogscheduler/SuperFrogSchedulerApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.superfrogscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperFrogSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperFrogSchedulerApplication.class, args);
+    }
+
+}
