0065bc43481792cdb2fb29a6c90dcebbccce7b9e
John Nguyen <j4nguyen134@gmail.com>
2023-04-17 10:30:53 -0500

    working findAll no test

--- SuperFrogScheduler/src/main/java/edu/tcu/cs/superfrogscheduler/controller/SuperFrogAppearanceController.java
+++ SuperFrogScheduler/src/main/java/edu/tcu/cs/superfrogscheduler/controller/SuperFrogAppearanceController.java
@@ -7,11 +7,12 @@ import edu.tcu.cs.superfrogscheduler.superfrogrequest.converter.SuperFrogAppeara
 import edu.tcu.cs.superfrogscheduler.superfrogrequest.dto.SuperFrogAppearanceRequestDto;
 import edu.tcu.cs.superfrogscheduler.system.HttpStatusCode;
 import edu.tcu.cs.superfrogscheduler.system.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.HTML;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SuperFrogAppearanceController {
@@ -28,10 +29,28 @@ public class SuperFrogAppearanceController {
         this.superFrogAppearanceRequestDtoToSuperFrogAppearanceRequestConverter = superFrogAppearanceRequestDtoToSuperFrogAppearanceRequestConverter;
     }
 
-    @GetMapping("api/superfrogappearancerequests/{requestId}")
+    @GetMapping("/api/superfrogappearancerequests/{requestId}")
     public Result findSuperFrogAppearanceById(@PathVariable int requestId) {
         SuperFrogAppearanceRequest foundSuperFrogAppearance = this.superFrogAppearanceRequestService.findById(requestId);
         SuperFrogAppearanceRequestDto superFrogAppearanceRequestDto = this.superFrogAppearanceRequestToSuperFrogAppearanceRequestDtoConverter.convert(foundSuperFrogAppearance);
         return new Result(true, HttpStatusCode.SUCCESS, "Find One Success", superFrogAppearanceRequestDto);
+    }
+
+    @GetMapping("/api/superfrogappearancerequests")
+    public Result findAllSuperFrogAppearances() {
+        List<SuperFrogAppearanceRequest> foundAppearance = this.superFrogAppearanceRequestService.findAll();
+
+        List<SuperFrogAppearanceRequestDto> appearanceRequestDtos = foundAppearance.stream()
+                .map(this.superFrogAppearanceRequestToSuperFrogAppearanceRequestDtoConverter::convert)
+                .collect(Collectors.toList());
+        return new Result(true, HttpStatusCode.SUCCESS, "Find All Success", appearanceRequestDtos);
+    }
+
+    @PostMapping("/api/superfrogappearancerequests")
+    public Result addSuperFrogAppearanceRequest(@Valid @RequestBody SuperFrogAppearanceRequestDto appearanceRequestDto) {
+        SuperFrogAppearanceRequest newAppearance = this.superFrogAppearanceRequestDtoToSuperFrogAppearanceRequestConverter.convert(appearanceRequestDto);
+        SuperFrogAppearanceRequest savedAppearance = this.superFrogAppearanceRequestService.save(newAppearance);
+        SuperFrogAppearanceRequestDto savedAppearanceDto = this.superFrogAppearanceRequestToSuperFrogAppearanceRequestDtoConverter.convert(savedAppearance);
+        return new Result(true, HttpStatusCode.SUCCESS, "Add Success", savedAppearanceDto);
     }
 }
