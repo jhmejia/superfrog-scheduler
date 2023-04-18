29cf8eece8cfae25f4147ef8508be025476c3506
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-04-18 12:36:16 -0500

    created maven

---
+++ superfrogscheduler/src/test/java/edu/tcu/cs/superfrogscheduler/SuperfrogschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.superfrogscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperfrogschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
