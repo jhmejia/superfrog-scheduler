81d9323eb6ac45e3f07d3b7456f6c6eee0288702
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-04-23 23:07:13 -0500

    added css to form

--- superfrogstudentscheduler/src/components/request-page/detail-form-page.vue
+++ superfrogstudentscheduler/src/components/request-page/detail-form-page.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
-      <div class="date"></div>
+      <div class="date-1"></div>
       <div class="contact-info">
-        <span class="title">Personal Contact</span>
+        <span class="title-1">Personal Contact</span>
         <div>
           <span>Contact First Name</span>
           <span><input type="text" v-model="eventInfo.contactFirstName" @input="updateParent"></span>
@@ -52,3 +52,44 @@
   }
   }
   </script>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: sans-serif;
+}
+
+.date-1 {
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.contact-info {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.title-1 {
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-bottom: 10px;
+  display: block;
+}
+
+div > span {
+  display: inline-block;
+  width: 200px;
+  margin-bottom: 10px;
+  font-weight: bold;
+}
+
+div > span input[type="text"] {
+  width: 185px;
+  padding: 5px;
+  border: 1px solid #ccc;
+}
+
+</style>
