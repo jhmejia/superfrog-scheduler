92820cdd48aa65a374eab66dcee53b3c9e5706cf
John Henry <johnhmejia02@gmail.com>
2023-04-23 23:54:32 -0500

    Update topbar.vue

--- superfrogstudentscheduler/src/components/admin-page/topbar.vue
+++ superfrogstudentscheduler/src/components/admin-page/topbar.vue
@@ -1,34 +1,56 @@
 <template>
-  <div class="top-bar">
-    <p>{{ username }}</p>
-  </div>
+    <div class="top-bar">
+        <div class="profile-image-container">
+            <img
+                class="profile-image"
+                src="https://ia903204.us.archive.org/4/items/discordprofilepictures/discordblue.png"
+                alt="Profile Image"
+            />
+        </div>
+
+        <p>{{ username }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    username: {
-      type: String,
-      required: true,
+    props: {
+        username: {
+            type: String,
+            required: true,
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 .top-bar {
-  height: 50px;
-  background-color: #f5f5f5;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  font-size: 24px;
-  font-weight: bold;
-  width: 100%;
-  padding-right: 10px; /* add some padding to the right */
+    height: 50px;
+    background-color: #f5f5f5;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-weight: bold;
+    width: 100%;
+    padding-right: 10px; /* add some padding to the right */
 }
 
 .top-bar p {
-  margin: 0; /* remove default margin */
+    margin: 0; /* remove default margin */
+}
+
+profile-image-container {
+    height: 50px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    display: flex;
+    align-items: center;
+}
+
+.profile-image {
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
 }
 </style>
