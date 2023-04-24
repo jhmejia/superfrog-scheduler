ad145a8607254e718136ea53812e8d4fa67561e0
John Henry <johnhmejia02@gmail.com>
2023-04-24 18:06:21 -0500

    try to get back end idk

--- superfrogstudentscheduler/src/components/admin-page/main-container.vue
+++ superfrogstudentscheduler/src/components/admin-page/main-container.vue
@@ -1,49 +1,70 @@
 <template>
-  <div class="main-box">
-    <h1>{{ section }}</h1>
-    <div v-if="section === 'add-student'">
-      <!-- add student form here -->
-      Add dat backend to add a student. Yo 
+    <div class="main-box">
+        <h1>{{ section }}</h1>
+        <div v-if="section === 'add-student'">
+            <!-- add student form here -->
+            Add dat backend to add a student. Yo
+        </div>
+        <div v-if="section === 'view-students'">
+            <p id="all-student"> All Students </p>
+        </div>
+        <div v-if="section === 'add-request'">
+            <!-- Add request form here -->
+            Add dat backend to add a request. Yo
+        </div>
+        <div v-if="section === 'view-requests'">
+            <!-- Display all requests here -->
+            Yo, I need to add the backend to view all requests, can't do it
+            alone so I'm askin' for assistance.
+        </div>
+        <div v-if="section === 'view-calendar'">
+            <!-- Display calendar here -->
+            Yo, I need to add the backend to view the calendar, can't do it
+            alone so I'm askin' for assistance.
+        </div>
+        <div v-if="section === 'view-reports'">
+            <!-- Display reports here -->
+            Yo, I need to add the backend to view the reports, can't do it alone
+            so I'm askin' for assistance.
+        </div>
     </div>
-    <div v-if="section === 'view-students'">
-      <!-- View all students here -->
-      Yo, I need to add the backend to view all students, can't do it alone so I'm askin' for assistance.
-    </div>
-    <div v-if="section === 'add-request'">
-      <!-- Add request form here -->
-      Add dat backend to add a request. Yo
-    </div>
-    <div v-if="section === 'view-requests'">
-      <!-- Display all requests here -->
-      Yo, I need to add the backend to view all requests, can't do it alone so I'm askin' for assistance.
-    </div>
-    <div v-if="section === 'view-calendar'">
-      <!-- Display calendar here -->
-      Yo, I need to add the backend to view the calendar, can't do it alone so I'm askin' for assistance.
-    </div>
-    <div v-if="section === 'view-reports'">
-      <!-- Display reports here -->
-      Yo, I need to add the backend to view the reports, can't do it alone so I'm askin' for assistance.
-    </div> 
-
-  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  props: {
-    section: {
-      type: String,
-      default: "students",
+    props: {
+        section: {
+            type: String,
+            default: "students",
+        },
     },
-  },
+    data() {
+        return {
+            students: [],
+        };
+    },
+    mounted() {
+        if (this.section === "view-students") {
+            this.getStudents();
+        }
+        
+    },
+    methods: {
+        getStudents() {
+            const base_url = "http://localhost:5000/api/v1/students";
+            axios
+                .get(base_url)
+                .then((response) => {
+                    console.log(response.data);
+                    this.students = response.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                    this.students = "Can't find students";
+                });
+        },
+    },
 };
 </script>
-
-<style scoped>
-.main-box {
-  width: 100%;
-  height: 100%;
-  padding: 10px;
-}
-</style>
