9631b462160110d3a3e7f9a999473c3af119335f
John Henry <johnhmejia02@gmail.com>
2023-04-24 17:50:06 -0500

    barebones login page

--- superfrogstudentscheduler/src/components/login-page/main-container.vue
+++ superfrogstudentscheduler/src/components/login-page/main-container.vue
@@ -1,34 +1,48 @@
 <template>
+    <div>
+        <form>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" /><br /><br />
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password" /><br /><br />
+        </form>
 
-    <form>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username"><br><br>
-        <label for="password">Password:</label>
-        <input type="text" id="password" name="password"><br><br>
-    </form>
+        <button type="button" class="btn btn-primary">
+            Login as SuperFrog
+        </button>
+        <button type="button" class="btn btn-primary">
+            Login as Spirit Director
+        </button>
 
-    <button type="button" class="btn btn-primary">Login as SuperFrog</button>
-    <button type="button" class="btn btn-primary">Login as Spirit Director</button>
-
+        <div class="home">  
+            <button type="button" class="btn btn-secondary" v-on:click="goHome">
+                Go Back
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'LoginPage',
-        data() {
-            return {
-                username: '',
-                password: ''
-            }
-        }
-    }
+export default {
+    name: "LoginPage",
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        goHome() {
+            this.$router.push("/");
+        },
+    },
+    
+};
 </script>
 
 <style scoped>
-    form {
-        margin: 0 auto;
-        width: 400px;
-    }
+form {
+    margin: 0 auto;
+    width: 400px;
+}
 </style>
-
-
