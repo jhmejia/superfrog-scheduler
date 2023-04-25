9df865ba8b9ae769c0c9bb72a078ab58a891641e
John Henry <johnhmejia02@gmail.com>
2023-04-25 14:55:02 -0500

    add range page.
    I got fed up with trying to work with the google maps api .

---
+++ superfrogstudentscheduler/src/components/range-page/map.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="map-container">
+        <div id="map">
+            <iframe
+                width="600"
+                height="450"
+                style="border: 0"
+                loading="lazy"
+                allowfullscreen
+                src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJFV0auj5yToYRgzlMof_OSfo&key=AIzaSyADl1BSYCI5OWl9n7Ppw4cJ5nMo3yEFA2c"
+            ></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GoogleMap",
+};
+</script>
+
+<style scoped>
+.map-container {
+    height: 100%;
+}
+
+#map {
+    height: 100%;
+}
+</style>
