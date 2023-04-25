a29f30e947cb147b96e758707eafe42945405885
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-04-24 22:53:36 -0500

    login page done

--- superfrogstudentscheduler/src/components/login-page/main-container.vue
+++ superfrogstudentscheduler/src/components/login-page/main-container.vue
@@ -1,27 +1,40 @@
 <template>
-    <div>
+    <div class="login-page">
+        <div class="landing-page">
+            <div class="profile-image-admin"></div>
+            <h1 class="heading-admin">
+                SuperFrog Scheduler Login Page
+            </h1> <br>
+            <!-- <h2>
+                Admin Login 
+            </h2> -->
+
+        </div>
+        <h2>Enter your SuperFrog/Spirit Director Username and Password</h2>
         <form>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" /><br /><br />
-            <label for="password">Password:</label>
-            <input type="text" id="password" name="password" /><br /><br />
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" />
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" />
+            </div>
         </form>
 
-        <button type="button" class="btn btn-primary">
-            Login as SuperFrog
-        </button>
-        <button type="button" class="btn btn-primary">
-            Login as Spirit Director
-        </button>
-
-        <div class="home">  
-            <button type="button" class="btn btn-secondary" v-on:click="goHome">
-                Go Back
+        <div class="login-buttons">
+            <button type="button" class="btn btn-primary">
+                Login as SuperFrog
+            </button>
+            <button type="button" class="btn btn-secondary">
+                Login as Spirit Director
             </button>
         </div>
+        <router-link to="/">
+        <button class="button is-primary home-button">Home</button>
+      </router-link>
     </div>
 </template>
-
 <script>
 export default {
     name: "LoginPage",
@@ -36,13 +49,124 @@ export default {
             this.$router.push("/");
         },
     },
-    
+
 };
 </script>
 
 <style scoped>
-form {
-    margin: 0 auto;
-    width: 400px;
+.login-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
 }
+
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    width: 400px;
+    margin-bottom: 20px;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    bottom: 500px;
+}
+
+label {
+    font-weight: bold;
+    color: black;
+}
+
+input[type="text"],
+input[type="password"] {
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    width: 70%;
+    font-size: 16px;
+}
+
+
+
+button {
+    font-size: 20px;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+    border-radius: 40px;
+    display: inline-block;
+    margin-bottom: 200px;
+}
+
+.btn-primary {
+    background-color: white;
+    width: 200px;
+}
+
+/* .btn-secondary {
+    background-color: white;
+} */
+
+.btn-primary:hover,
+.btn-secondary:hover {
+    opacity: 0.8;
+}
+
+.btn-secondary {
+    margin-right: 0;
+}
+
+.login-buttons {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px; /* add margin to bottom to create space between the two button groups */
+}
+
+.home {
+  display: flex;
+  justify-content: center;
+}
+
+.profile-image-admin {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    border-radius: 50%;
+    width: 250px;
+    height: 250px;
+    background-image: url("https://gofrogs.com/common/controls/image_handler.aspx?thumb_id=12&image_path=/images/2018/9/26/SuperFrogSITE_crop_71.jpg");
+    background-size: cover;
+    background-position: center;
+}
+
+.heading-admin {
+    font-family: "Times New Roman", Times, serif;
+    color: white;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    font-size: 50px;
+    font-weight: 2000px;
+}
+button.button.is-primary {
+  width: 120px;
+}
+
 </style>
