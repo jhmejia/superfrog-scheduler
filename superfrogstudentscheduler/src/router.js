5cc90a6c425a9d5e259d54a51b956d37e4d582e4
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-04-24 20:14:23 -0500

    added login page

--- superfrogstudentscheduler/src/router.js
+++ superfrogstudentscheduler/src/router.js
@@ -1,18 +1,25 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
-import PricingPage from '@/components/pricing-page/main-container.vue'
+// import PricingPage from '@/components/pricing-page/main-container.vue'
+import AdminPage from '@/components/admin-page/main-parent.vue'
 import RequestPage from '@/components/request-page/main-container.vue'
 import PricingPage from '@/components/pricing-page/main-container.vue'
+import LoginPage from '@/components/login-page/main-container.vue'
+import SuperfrogPage from '@/components/superfrog-page/main-parent.vue'
 import App from '@/App.vue'
+
 
 const routes = [
     { path: '/', component: App },
 
     { path: '/pricing', component: PricingPage },
+    { path: '/admin', component: AdminPage},
+    { path: '/superfrog', component: SuperfrogPage},
 
     { path: '/range', component: RequestPage },
     { path: '/request', component: RequestPage },
     { path: '/modify-request', component: RequestPage },
+    { path: '/login', component: LoginPage },
     { path: '/:catchAll(.*)',
     component: App}
 ]
