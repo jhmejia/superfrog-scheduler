f10bfb67182cff5bf24c3aa4bcb91a3e34e75b4c
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-04-30 17:02:43 -0500

    Completed button added

--- superfrogstudentscheduler/src/components/superfrog-page/view-requests.vue
+++ superfrogstudentscheduler/src/components/superfrog-page/view-requests.vue
@@ -36,7 +36,9 @@
                         <button @click="approveRequest(request.requestId)">
                             Sign-Up
                         </button>
-                        <!-- <button @click="rejectRequest(request)">Reject</button> -->
+                        <button v-if="request.status === 'APPROVED'" @click="completeRequest(request.requestId)">
+                            Completed
+                        </button>
                     </td>
                 </tr>
             </tbody>
@@ -126,6 +128,50 @@ export default {
                     console.log(error);
                 });
         },
+        completeRequest(requestId) {
+        const token = localStorage.getItem("token");
+        const headers = {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        };
+        // TODO: Implement complete request functionality
+        const superfrogEmail = localStorage.getItem("superfrogEmail");
+        let superFrogId = "";
+
+        //Get all superfrog students
+        axios
+            .get("http://localhost:8080/api/superfrogstudents")
+            .then((response) => {
+                for (let i = 0; i < response.data.data.length; i++) {
+                    if (response.data.data[i].email === superfrogEmail) {
+                        superFrogId = response.data.data[i].id;
+                        console.log("Superfrog id is ");
+                        console.log(this.superFrogId);
+                        console.log("Request id is ");
+                        console.log(requestId);
+
+                        break;
+                    }
+                }
+
+                axios
+                    .put(
+                        `http://localhost:8080/api/superfrogappearancerequests/${requestId}/status/COMPLETED`,
+                        null,  { headers }
+                    )
+                    .then((response) => {
+                        this.requests = response.data.data;
+                        console.log(response.data.data);
+                        this.getRequests();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
     },
     computed: {
         computedRequests() {
