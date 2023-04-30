b7ad6f85b9f1757b62049c7dae372cd8d4c714d1
John Nguyen <j4nguyen134@gmail.com>
2023-04-30 18:22:26 -0500

    reassign button in view requests

--- superfrogstudentscheduler/src/components/admin-page/view-requests.vue
+++ superfrogstudentscheduler/src/components/admin-page/view-requests.vue
@@ -5,6 +5,7 @@
       <option value="">All</option>
       <option value="APPROVED">Approved</option>
       <option value="PENDING">Pending</option>
+      <option value="COMPLETED">Completed</option>
       <option value="REJECTED">Rejected</option>
     </select>
 <p id="requests">Requests</p>
@@ -18,6 +19,7 @@
       <th>Request Status</th>
       <th>Assigned SuperFrog</th>
       <th>Action</th>
+      <th>Reassignment</th>
     </tr>
   </thead>
   <tbody>
@@ -31,6 +33,20 @@
       <td>
         <button @click="approveRequest(request)">Approve</button>
         <button @click="rejectRequest(request)">Reject</button>
+      </td>
+      <td>
+        <select
+              v-if="superfrogs.length"
+              id="superfrogs-select"
+              name="superfrog"
+              v-model="request.superfrogId"
+              @change="assignSuperFrog(request.requestId, request.superfrogId)"
+            >
+              <option value="">Select a SuperFrog</option>
+              <option v-for="superfrog in superfrogs" :value="superfrog.id">
+                {{ superfrog.name }}
+              </option>
+            </select>
       </td>
     </tr>
   </tbody>
@@ -46,10 +62,32 @@ export default {
   data() {
     return {
       requests: [],
+      superfrogs: [],
+      superfrogId: null,
     };
   },
   mounted() {
     this.getRequests();
+    axios
+            .get("http://localhost:8080/api/superfrogstudents/active", )
+            .then((response) => {
+                console.log(response.data);
+
+                //Get first name and last name of each superfrog
+                const superfrogs = response.data.data.map((superfrog) => {
+                    return {
+                        id: superfrog.id,
+                        name: superfrog.firstName + " " + superfrog.lastName,
+                    };
+                });
+
+                this.superfrogs = superfrogs;
+
+                console.log(this.superfrogs);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
   },
   methods: {
     getRequests() {
@@ -102,6 +140,23 @@ export default {
           console.log(error);
         });
     },
+    assignSuperFrog(requestId,superfrogId) {
+      const token = localStorage.getItem("token");
+      const headers = {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+      };
+      axios
+        .put(`http://localhost:8080/api/superfrogstudents/${superfrogId}/assign/superfrogappearancerequests/${requestId}`, {
+        }, { headers})
+        .then((response) => {
+          console.log(response.data.data);
+          this.getRequests();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   },
   computed: {
     computedRequests() {
