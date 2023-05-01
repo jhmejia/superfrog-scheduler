13eaf00e21b48b9e3d4acdd6ad40258cbd6f9fe6
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-05-01 01:42:25 -0500

    added success message

--- superfrogstudentscheduler/src/components/superfrog-page/edit-profile.vue
+++ superfrogstudentscheduler/src/components/superfrog-page/edit-profile.vue
@@ -1,33 +1,3 @@
-<!-- <template>
-    <div>
-        <form @submit.prevent="saveChanges">
-            <div>
-                <label for="firstName">First Name</label>
-                <input type="text" v-model="superFrog.firstName" id="firstName" required />
-            </div>
-            <div>
-                <label for="lastName">Last Name</label>
-                <input type="text" v-model="superFrog.lastName" id="lastName" required />
-            </div>
-            <div>
-                <label for="email">Email</label>
-                <input type="email" v-model="superFrog.email" id="email" required />
-            </div>
-            <div>
-                <label for="phoneNumber">Phone Number</label>
-                <input type="text" v-model="superFrog.phoneNumber" id="phoneNumber" />
-            </div>
-            <div>
-                <label for="status">Active Status:</label>
-                <select id="status" v-model="superFrog.active">
-                    <option value="true">Active</option>
-                    <option value="false">Inactive</option>
-                </select>
-            </div>
-            <button type="submit">Save Changes</button>
-        </form>
-    </div>
-</template> -->
 <template>
     <div>
       <form @submit.prevent="saveChanges">
@@ -56,6 +26,8 @@
         </div> -->
         <button type="submit">Save Changes</button>
       </form>
+      <p v-if="successMessage">{{ successMessage }}</p>
+      <p v-if="superfrogSuccessMessage">{{ superfrogSuccessMessage }}</p>
     </div>
   </template>
   
@@ -67,6 +39,7 @@ export default {
     data() {
         return {
             superFrog: {},
+            successMessage: null,
         };
     },
     mounted() {
@@ -106,10 +79,12 @@ export default {
                 })
                 .then((response) => {
                     console.log(response.data.data);
-                    // Redirect to profile page or show success message
+                    this.successMessage = "Request edited successfully!";
                 })
                 .catch((error) => {
                     console.log(error);
+                    this.successMessage =
+                        "Request not edited successfully- please try again";
                 });
         },
     },
