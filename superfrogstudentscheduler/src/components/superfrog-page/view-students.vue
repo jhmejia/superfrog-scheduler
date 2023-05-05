ced32b9f2b02242eca108b845ed067db04979dd7
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-05-04 22:45:46 -0500

    added page

--- superfrogstudentscheduler/src/components/superfrog-page/view-students.vue
+++ superfrogstudentscheduler/src/components/superfrog-page/view-students.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <p id="view-students"> Students </p>
+    <label>
+      <input type="checkbox" v-model="showActiveOnly"> Show active students only
+    </label>
     <table>
       <thead>
         <tr>
@@ -26,80 +29,118 @@
         </tr>
       </tbody>
     </table>
+    <div class="pagination">
+      <button :disabled="currentPage === 1" @click="prevPage">Prev</button>
+      <span>{{ currentPage }} / {{ totalPages }}</span>
+      <button :disabled="currentPage === totalPages" @click="nextPage">Next</button>
+    </div>
   </div>
 </template>
   
-  <script>
-  import axios from "axios";
-  
-  export default {
-    name: "ViewStudents",
-    data() {
-      return {
-        students: [],
-      };
+<script>
+import axios from "axios";
+
+export default {
+  name: "ViewStudents",
+  data() {
+    return {
+      students: [],
+      showActiveOnly: false,
+      currentPage: 1,
+      rowsPerPage: 5,
+    };
+  },
+  mounted() {
+    this.getStudents();
+  },
+  methods: {
+    getStudents() {
+      axios
+        .get("http://206.189.255.67:8080/api/superfrogstudents")
+        .then((response) => {
+          this.students = response.data.data;
+          console.log(response.data.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    mounted() {
-      this.getStudents();
+    toggleStudentStatus(student) {
+      const url = `http://localhost:8080/api/users/${student.email}/${student.active ? 'disable' : 'enable'}`;
+      const token = localStorage.getItem('token');
+
+      console.log(token);
+
+      const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+      axios
+        .put(url, {}, { headers })
+        .then((response) => {
+          console.log(response.data)
+          this.getStudents();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-      getStudents() {
-        axios
-          .get("http://206.189.255.67:8080/api/superfrogstudents")
-          .then((response) => {
-            this.students = response.data.data;
-            console.log (response.data.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-      toggleStudentStatus(student) {
-        const url = `http://localhost:8080/api/users/${student.email}/${student.active ? 'disable' : 'enable'}`;
-        const token = localStorage.getItem('token');
-        
-        console.log(token);
-        
-        const headers = {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-        axios
-          .put(url, {}, { headers })
-          .then((response) => {
-            console.log(response.data)
-            this.getStudents();
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
     },
-    computed: {
-      computedStudents() {
-        return this.students;
-      },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
     },
-  };
-  </script>
-  <style scoped>
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
-  
-  th, td {
-    text-align: left;
-    padding: 8px;
-  }
-  
-  th {
-    background-color: #4CAF50;
-    color: white;
-  }
-  
-  tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
-  
-  </style>
+  },
+  computed: {
+    computedStudents() {
+      return this.students;
+    },
+    totalPages() {
+      let students = [];
+      if (this.showActiveOnly) {
+        students = this.students.filter(student => student.active);
+      } else {
+        students = this.students;
+      }
+      return Math.ceil(students.length / this.rowsPerPage);
+    },
+    displayedStudents() {
+      let students = [];
+      if (this.showActiveOnly) {
+        students = this.students.filter(student => student.active);
+      } else {
+        students = this.students;
+      }
+      const startIndex = (this.currentPage - 1) * this.rowsPerPage;
+      const endIndex = startIndex + this.rowsPerPage;
+      return students.slice(startIndex, endIndex);
+    },
+  },
+};
+</script>
+<style scoped>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  text-align: left;
+  padding: 8px;
+}
+
+th {
+  background-color: #4CAF50;
+  color: white;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+</style>
